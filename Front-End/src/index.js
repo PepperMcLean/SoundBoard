8cb21fffdc60f0df66570b7f41ca507413d073a6
PepperMcLean <jackmcl206@gmail.com>
2022-01-20 01:59:53 -0600

    first button

---
+++ Front-End/src/index.js
@@ -0,0 +1,5 @@
+const buttonLogin = document.getElementById("login")
+const buttonSignup = document.getElementById("signup")
+
+buttonLogin.addEventListener('click', function(){console.log("itworks")});
+buttonSignup.addEventListener('click', function(){console.log("itworks2")});
